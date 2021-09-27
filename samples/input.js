04cf657f9465c5e69576aa9726a749367bd9f9e3
mcaikovs <marian.caikovski@gmail.com>
2021-09-27 10:11:00 +0200

    first

--- samples/input.js
+++ samples/input.js
@@ -6,29 +6,23 @@ function input({ list, ...props }) {
     return el;
 }
 
-function addInput() {
-    root.appendChild(input({
-        value: 'MyValue',
-        placeholder: 'MyPlaceholder',
-        name: 'MyName',
-        title: 'MyTitle',
-        className: 'MyClass',
-        list: 'browsers',
-        type: 'search',
-        pattern: '[a-z]+',
-        required: true,
-        autocomplete: 'off',
-    }));
+root.appendChild(input({
+    value: 'MyValue',
+    placeholder: 'MyPlaceholder',
+    name: 'MyName',
+    title: 'MyTitle',
+    className: 'MyClass',
+    list: 'browsers',
+    type: 'search',
+    pattern: '[a-z]+',
+    required: true,
+    autocomplete: 'off',
+}));
 
-    root.appendChild(input({
-        name: 'MyName',
-        title: 'MyTitle',
-        className: 'MyClass',
-        type: 'checkbox',
-        checked: true
-    }))
-}
-
-addInput();
-// const divProps = { clazz: 'row', val: 'Hello' };
-// const html = `<div class=${}>`
+root.appendChild(input({
+    name: 'MyName',
+    title: 'MyTitle',
+    className: 'MyClass',
+    type: 'checkbox',
+    checked: true
+}))
